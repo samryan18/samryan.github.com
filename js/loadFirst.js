d353a517be86c84b362b04bb9284b2bc137594ca
Sam <saml.ryan@yahoo.com>
2018-02-26 21:30:58 -0500

    test

--- js/loadFirst.js
+++
@@ -1,9 +0,0 @@
-
-
-// Loads the whole document with a white screen then shows screen once loaded
-
-document.getElementById("hideAll").style.display = "block";
-
-window.onload = function() { 
-  document.getElementById("hideAll").style.display = "none"; 
-} 
