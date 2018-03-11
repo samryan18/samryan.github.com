803e1b8c8539ef5cf4d2aed0e69c63981c77ad79
Sam <saml.ryan@yahoo.com>
2018-03-11 15:57:51 -0400

    contact

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,6 +1,6 @@
 
 function submit() {
-    window.alert("github pages doesn't allow me to send post requests :/ workin on it ");
+    window.alert("github pages doesn't allow me to send post requests :/ workin on it \n\nemail -> samryan@seas.upenn.edu");
 }
 
 $(".special").click(submit)
