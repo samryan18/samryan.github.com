208cab2ced9778525ce27cc06e28d6485b0d9be8
Sam <saml.ryan@yahoo.com>
2018-08-15 14:41:23 -0700

    clean

--- assets/js/load-pictures.js
+++ assets/js/load-pictures.js
@@ -1,194 +1,66 @@
 /* Only loads pictures after home page loads so page loads faster */
 
-var pictureHTML = `
-								
+PATH = "assets/images/"
+function buildHTML(fileName, location = "", path = PATH) {
+	return `			
 						        <span class="image main">
-									<figure><img src="assets/images/GlenPark.jpg" alt="">
-								    <figcaption>Glen Park, SF</figcaption></figure>
-								</span>
-								
-								<span class="image main">
-									<figure><img src="assets/images/StrawberryMansion.jpg" alt="">
-								    <figcaption>Strawberry Mansion Bridge, Philly</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/strawberryMansion3.jpg" alt="">
-								    <figcaption>Strawberry Mansion Bridge, Philly</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/strawberryMansion2.jpg" alt="">
-								    <figcaption>Strawberry Mansion Bridge, Philly</figcaption></figure>
-								</span> ` +
-								// <span class="image main">
-								// 	<figure><img src="assets/images/overlay.png" alt=""/>
-								//     <figcaption>Peace Valley, PA (Evan Jonokuchi)</figcaption></figure>
-								// </span>
-								` <span class="image main">
-									<figure><img src="assets/images/views.JPG" alt="" >
-								    <figcaption>Grand Canyon</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/eclipse.jpg" alt="">
-									<figcaption>Borah Peak, ID</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/bus.JPG" alt="">
-								    <figcaption>Walmart Parking Lot, Tennessee</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/MayLake.jpg" alt="">
-								    <figcaption>May Lake, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/may3.JPG" alt="">
-								    <figcaption>May Lake, CA</figcaption></figure>
-								</span>
-								<!-- <span class="image main">
-									<figure><img src="assets/images/campsite.jpg" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span> -->
-								<span class="image main">
-									<figure><img src="assets/images/knox.JPG" alt="">
-								    <figcaption>SF, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/Amoudi.jpg" alt="">
-								    <figcaption>Amoudi Bay, Greece</figcaption></figure>
-								</span> ` +
-								// <span class="image main">
-								// 	<figure><img src="assets/images/everything.jpg" alt="">
-								//     <figcaption>SF, CA (Evan Jonokuchi)</figcaption></figure>
-								// </span>
-								` <span class="image main">
-									<figure><img src="assets/images/halfDome.JPG" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								
-								<span class="image main">
-									<figure><img src="assets/images/sutro.JPG" alt="">
-								    <figcaption>SF, CA</figcaption></figure>
-								</span>
-								
-								<span class="image main">
-									<figure><img src="assets/images/campsite2.JPG" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/may2.JPG" alt="">
-								    <figcaption>May Lake, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/el_cap.jpg" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/half_dome_dark.jpg" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/theRoad.JPG" alt="">
-								    <figcaption>Tennessee</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/alienPlant.JPG" alt="">
-								    <figcaption>Grand Canyon</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/morning.JPG" alt="">
-								    <figcaption>Clingman's Dome, TN</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/myRoom.JPG" alt="">
-								    <figcaption>SF, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/halfDome2.jpg" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/athens.jpg" alt="">
-								    <figcaption>Athens, Greece</figcaption></figure>
-								</span>
-								
-								<span class="image main">
-									<figure><img src="assets/images/beach.jpg" alt="">
-								    <figcaption>Sauni, Greece</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/MtDave.jpg" alt="">
-								    <figcaption>Mt. Dave, SF</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/allGreen.jpg" alt="">
-								    <figcaption>Yosemite, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/tree.jpg" alt="">
-								    <figcaption>Poopenaut Valley, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/dude.jpg" alt="">
-								    <figcaption>SF, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/sprangBrake.jpg" alt="">
-								    <figcaption>GW Natl. Forest, VA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/peaceValley2.jpg" alt="">
-								    <figcaption>Peace Valley, PA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/fallsBridge.jpg" alt="">
-								    <figcaption>Falls Bridge, Philly</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/Tenaya.jpg" alt="">
-								    <figcaption>Tenaya Canyon, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/smile.jpg" alt="">
-								    <figcaption>Fairmount Park, Philly</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/ryanWomen.jpg" alt="">
-								    <figcaption>Outside Chicago, IL</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/Santorini.JPG" alt="">
-								    <figcaption>Santorini, Greece</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/edinburgh.JPG" alt="">
-								    <figcaption>Edinburgh, Scotland</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/lakeDistrict.JPG" alt="">
-								    <figcaption>Lake District, UK</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/johnChess.JPG" alt="">
-								    <figcaption>Glen Park, SF</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/ggb.JPG" alt="">
-								    <figcaption>SF, CA</figcaption></figure>
-								</span>
-								<span class="image main">
-									<figure><img src="assets/images/hammock.JPG" alt="">
-								    <figcaption>Russian River, CA</figcaption></figure>
-								</span>
-								<!-- <span class="image main">
-									<figure><img src="assets/images/goodVibes.JPG" alt="" >
-								    <figcaption></figcaption></figure>
-								</span> -->
-								<!-- <span class="image main">
-									<figure><img src="assets/images/CoitTower.JPG" alt="" >
-								    <figcaption></figcaption></figure>
-								</span> -->
-`
+									<figure><img src="` + path + fileName + `" alt="">
+								    <figcaption>` + location + `</figcaption></figure>
+								</span>`;
+}
 
+var picture_files = {
+	"GlenPark.jpg": "Glen Park, SF",
+	"StrawberryMansion.jpg": "Strawberry Mansion Bridge, Philly",
+	"strawberryMansion3.jpg": "Strawberry Mansion Bridge, Philly",
+	"strawberryMansion2.jpg": "Strawberry Mansion Bridge, Philly",
+	"views.JPG": "Grand Canyon",
+	"eclipse.jpg": "Borah Peak, ID",
+	"bus.JPG": "Walmart Parking Lot, Tennessee",
+	"MayLake.jpg": "May Lake, CA",
+	"may3.JPG": "May Lake, CA",
+	"campsite.jpg": "Yosemite, CA",
+	"knox.JPG": "SF, CA",
+	"Amoudi.jpg": "Amoudi Bay, Greece",
+	"halfDome.JPG": "Yosemite, CA",
+	"sutro.JPG": "SF, CA",
+	"campsite2.JPG": "Yosemite, CA",
+	"may2.JPG": "May Lake, CA",
+	"el_cap.jpg": "Yosemite, CA",
+	"half_dome_dark.jpg": "Yosemite, CA",
+	"theRoad.JPG": "Tennessee",
+	"alienPlant.JPG": "Grand Canyon",
+	"morning.JPG": "Clingman's Dome, TN",
+	"myRoom.JPG": "SF, CA",
+	"halfDome2.jpg": "Yosemite, CA",
+	"athens.jpg": "Athens, Greece",
+	"beach.jpg": "Sauni, Greece",
+	"MtDave.jpg": "Mt. Dave, SF",
+	"allGreen.jpg": "Yosemite, CA",
+	"tree.jpg": "Poopenaut Valley, CA",
+	"dude.jpg": "SF, CA",
+	"sprangBrake.jpg": "GW Natl. Forest, VA",
+	"peaceValley2.jpg": "Peace Valley, PA",
+	"fallsBridge.jpg": "Falls Bridge, Philly",
+	"Tenaya.jpg": "Tenaya Canyon, CA",
+	"smile.jpg": "Fairmount Park, Philly",
+	"ryanWomen.jpg": "Outside Chicago, IL",
+	"Santorini.JPG": "Santorini, Greece",
+	"edinburgh.JPG": "Edinburgh, Scotland",
+	"lakeDistrict.JPG": "Lake District, UK",
+	"johnChess.JPG": "Glen Park, SF",
+	"ggb.JPG": "SF, CA",
+	"hammock.JPG": "Russian River, CA"
+};
+
+
+var pictureHTML = ``
+for (var fileName in picture_files) {
+	if (picture_files.hasOwnProperty(fileName)) {
+		pictureHTML += buildHTML(fileName, picture_files[fileName]);
+	}
+}
 
 window.onload = function () {
-    $("#pics").append(pictureHTML)
+	$("#pics").append(pictureHTML)
 };
